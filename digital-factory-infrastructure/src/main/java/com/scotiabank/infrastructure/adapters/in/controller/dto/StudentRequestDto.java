09b4cefcaed5b68a14a9392f8298e407108b15b4
Paúl Guevara <pauljgl2008@gmail.com>
2024-04-28 21:21:23 -0500

    refactor: apply clean code

--- digital-factory-infrastructure/src/main/java/com/scotiabank/infrastructure/adapters/in/controller/dto/StudentRequestDto.java
+++ digital-factory-infrastructure/src/main/java/com/scotiabank/infrastructure/adapters/in/controller/dto/StudentRequestDto.java
@@ -6,33 +6,35 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
+import static com.scotiabank.infrastructure.adapters.in.controller.common.ValidationConstants.*;
+
 @Builder
 @Getter
 @Setter
 public class StudentRequestDto {
 
-    @NotBlank(message = "El campo id es requerido")
-    @Size(max = 8, message = "El campo id no debe exceder los 8 caracteres")
+    @NotBlank(message = STUDENT_ID_REQUIRED_MESSAGE)
+    @Size(max = 8, message = STUDENT_ID_LENGTH_EXCEEDED_MESSAGE)
     private String id;
 
-    @NotBlank(message = "El campo nombre es requerido")
-    @Size(max = 15, message = "El campo nombre no debe exceder los 10 caracteres")
-    @JsonProperty("nombre")
+    @NotBlank(message = STUDENT_NAME_REQUIRED_MESSAGE)
+    @Size(max = 15, message = STUDENT_NAME_LENGTH_EXCEEDED_MESSAGE)
+    @JsonProperty(NAME_PARAM)
     private String name;
 
-    @NotBlank(message = "El campo apellido es requerido")
-    @Size(max = 30, message = "El campo apellido no debe exceder los 30 caracteres")
-    @JsonProperty("apellido")
+    @NotBlank(message = STUDENT_LASTNAME_REQUIRED_MESSAGE)
+    @Size(max = 30, message = STUDENT_LASTNAME_LENGTH_EXCEEDED_MESSAGE)
+    @JsonProperty(LASTNAME_PARAM)
     private String lastname;
 
-    @NotNull(message = "El campo estado es requerido")
-    @JsonProperty("estado")
+    @NotNull(message = STUDENT_STATUS_REQUIRED_MESSAGE)
+    @JsonProperty(STATUS_PARAM)
     private String status;
 
-    @NotNull(message = "El campo edad es requerido")
-    @Min(value = 0, message = "El campo edad debe ser mayor o igual a 0")
-    @Max(value = 100, message = "El campo edad no debe exceder los 100 años")
-    @JsonProperty("edad")
+    @NotNull(message = STUDENT_AGE_REQUIRED_MESSAGE)
+    @Min(value = 0, message = STUDENT_AGE_MUST_BE_NON_NEGATIVE_MESSAGE)
+    @Max(value = 100, message = STUDENT_AGE_EXCEEDED_MAXIMUM_MESSAGE)
+    @JsonProperty(AGE_PARAM)
     private Integer age;
 
 }

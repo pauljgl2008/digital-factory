8ebc732f30bbcdce1cacce2c4b201401feb7615c
Paúl Guevara <pauljgl2008@gmail.com>
2026-02-23 15:19:12 -0500

    creation of project for digital factory

---
+++ src/main/java/com/scotiabank/DigitalFactoryApplication.java
@@ -0,0 +1,13 @@
+package com.scotiabank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalFactoryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalFactoryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scotiabank/DigitalFactoryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scotiabank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalFactoryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
